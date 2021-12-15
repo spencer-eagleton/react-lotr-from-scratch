4678798e7e1a23415ecb998725c498094c07ec58
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-14 17:06:51 -0800

    added necessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+// import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <BrowserRouter>
+        <header>
+          <NavLink>Home</NavLink>
+        </header>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </BrowserRouter> */}
+    </div>
+  );
 }
 
 export default App;
