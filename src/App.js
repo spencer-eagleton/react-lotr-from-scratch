cbe1bc3ba2701a9377301a6d375addb407ec6768
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-14 17:34:30 -0800

    added navlinks for views

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 import Main from './views/Main/Main';
+import Books from './views/Books/Books';
+import Characters from './views/Characters/Characters';
+import Films from './views/Films/Films';
 
 function App() {
   return (
@@ -8,10 +11,22 @@ function App() {
       <BrowserRouter>
         <header>
           <NavLink to="/">Home</NavLink>
+          <NavLink to="/books">Books</NavLink>
+          <NavLink to="/characters">Characters</NavLink>
+          <NavLink to="/films">Films</NavLink>
         </header>
         <Switch>
           <Route path="/">
             <Main />
+          </Route>
+          <Route path="/books">
+            <Books />
+          </Route>
+          <Route path="/characters">
+            <Characters />
+          </Route>
+          <Route path="/films">
+            <Films />
           </Route>
         </Switch>
       </BrowserRouter>
