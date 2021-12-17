4e6b85b909e30ca5c4fc3581aca2bc4be03a9bd0
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-16 16:00:11 -0800

    created controls file

---
+++ src/views/Characters/Controls.js
@@ -0,0 +1,15 @@
+export default function Controls() {
+  return (
+    <div>
+      <select name="race">
+        <option value="All">All</option>
+        <option value="Dwarf">Dwarf</option>
+        <option value="Elf">Elf</option>
+        <option value="Hobbit">Hobbit</option>
+        <option value="Human">Human</option>
+        <option value="Maiar">Maiar</option>
+        <option value="Orc">Orc</option>
+      </select>
+    </div>
+  );
+}
